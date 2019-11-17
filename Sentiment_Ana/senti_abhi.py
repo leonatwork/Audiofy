10de66928aa3098c0adb19dea1f6894cb877daaf
Abhijeet Mathur <abhijeetmathur786@gmail.com>
2019-11-17 15:22:48 +0530

    Trained and tested naive bayes on twitter data

--- Sentiment_Ana/senti_abhi.py
+++ Sentiment_Ana/senti_abhi.py
@@ -96,3 +96,51 @@ preprocessedTrainingSet = tweetProcessor.processTweets(trainingData)
 print(preprocessedTrainingSet)
 preprocessedTestSet = tweetProcessor.processTweets(testDataSet)
 print(preprocessedTestSet)
+
+import nltk 
+
+def buildVocabulary(preprocessedTrainingData):
+    all_words = []
+    
+    for (words, sentiment) in preprocessedTrainingData:
+        all_words.extend(words)
+
+    wordlist = nltk.FreqDist(all_words)
+    print(wordlist)
+    word_features = wordlist.keys()
+    print(word_features)
+    return word_features
+
+# ------------------------------------------------------------------------
+
+def extract_features(tweet):
+    tweet_words=set(tweet)
+    features={}
+    for word in word_features:
+        features['contains(%s)' % word]=(word in tweet_words)
+    return features 
+
+# ------------------------------------------------------------------------
+
+# Now we can extract the features and train the classifier 
+word_features = buildVocabulary(preprocessedTrainingSet)
+trainingFeatures=nltk.classify.apply_features(extract_features,preprocessedTrainingSet)
+
+NBayesClassifier=nltk.NaiveBayesClassifier.train(trainingFeatures)
+
+NBResultLabels = [NBayesClassifier.classify(extract_features(tweet[0])) for tweet in preprocessedTestSet]
+
+# ------------------------------------------------------------------------
+
+# get the majority vote
+if NBResultLabels.count('positive') > NBResultLabels.count('negative'):
+    print("Overall Positive Sentiment")
+    print("Positive Sentiment Percentage = " + str(100*NBResultLabels.count('positive')/len(NBResultLabels)) + "%")
+else: 
+    print("Overall Negative Sentiment")
+    print("Negative Sentiment Percentage = " + str(100*NBResultLabels.count('negative')/len(NBResultLabels)) + "%")
+
+
+#Ouput of above code
+#Overall Negative Sentiment
+# Negative Sentiment Percentage = 1%
