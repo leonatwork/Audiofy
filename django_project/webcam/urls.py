c7f51b3ab3c029d709721ba43a9de0ef3395f93c
leonatwork <noelabydas@gmail.com>
2021-02-20 11:16:13 +0530

    PDF view integration

--- django_project/webcam/urls.py
+++ django_project/webcam/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home page"),
-    path('camera', views.camVid, name="webcam page")
+    path('camera', views.camVid, name="webcam page"),
+    path('ebook', views.renderEbook, name="ebook page")
 ]
